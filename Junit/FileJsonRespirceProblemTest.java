e8bbb0b2223a8654e94c0fbe8c39128063bc1b41
mikewasouski <migel_mx@yahoo.com.mx>
2016-05-25 14:48:23 -0700

    implementation of the test unit to test collections readed from file done

--- Junit/FileJsonRespirceProblemTest.java
+++ Junit/FileJsonRespirceProblemTest.java
@@ -1,12 +1,50 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
+
+import com.comcast.csv.interview.problems.FileJsonResourceProblem;
+import com.comcast.csv.meme.Meme;
+import com.comcast.csv.meme.YoureDoingItWrongException;
 
 public class FileJsonRespirceProblemTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		List<Meme> memes = new ArrayList<Meme>();
+		Meme meme = new Meme();
+		meme.setName("Miguel Angel R");
+		meme.setYear(2005);
+		meme.setTags(new String[] {"One","Two","Three","Four","Mike"});
+		memes.add(meme);
+		
+		Meme meme1 = new Meme();
+		meme1.setName("Elizabeth Graham");
+		meme1.setYear(2007);
+		meme1.setTags(new String[] {"uno","dos","tres","cuatro","algo"});
+		memes.add(meme1);
+		
+		Meme meme2 = new Meme();
+		meme2.setName("reiko nakamura");
+		meme2.setYear(2005);
+		meme2.setTags(new String[] {"ein","zwei","drei","vier","prost"});
+		memes.add(meme2);
+		
+		List<Meme> memesFile = new ArrayList<Meme>();
+		FileJsonResourceProblem fileProblem = new FileJsonResourceProblem();
+		try {
+			memesFile = fileProblem.readFromClasspath("D:\\info.json");
+		} catch (YoureDoingItWrongException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		for(Meme memeAux : memes){
+			assertTrue(memesFile.contains(memeAux));
+		}
+		
 	}
 
 }
--- src/main/java/com/comcast/csv/meme/Meme.java
+++ src/main/java/com/comcast/csv/meme/Meme.java
@@ -1,5 +1,7 @@
 package com.comcast.csv.meme;
 
+import java.util.Arrays;
+
 /**
  * Simple class used by various tests.
  */
@@ -49,6 +51,34 @@ public class Meme {
      */
     public void setTags(String[] tags) {
         this.tags = tags;
+    }
+    
+    @Override
+    public int hashCode() {
+        int result = 0;
+        result = (int) (year / 7) + tags.length+ name.length();
+        return result;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+    	boolean result = false;
+    	
+    	if(obj instanceof Meme){
+    		Meme memeAux = (Meme) obj;
+    		
+    		
+    		if(this.name.equals(memeAux.getName()) && this.year == memeAux.getYear() && this.tags.length == tags.length){
+    			for(String tag : memeAux.getTags()){
+    				if(!Arrays.asList(this.tags).contains(tag)){
+    					return false;
+    				}
+    			}
+    			result = true;
+    		}
+    	}
+
+    	return result;
     }
 
 }
